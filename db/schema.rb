295a0f2e1c26fa48d80b8d0678ea5d770fa65269
Kenneth Young Castro <kenneth.young90@gmail.com>
2019-01-27 19:12:27 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -12,14 +12,6 @@
 
 ActiveRecord::Schema.define(version: 2019_01_27_175614) do
 
-  create_table "create_movies", force: :cascade do |t|
-    t.string "title"
-    t.integer "release_date"
-    t.string "director"
-    t.string "lead"
-    t.boolean "in_theaters"
-  end
-
   create_table "movies", force: :cascade do |t|
     t.string "title"
     t.integer "release_date"
