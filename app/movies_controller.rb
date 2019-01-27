26b25890a014663f2cfcdc40674f92bd23a1ada7
Kenneth Young Castro <kenneth.young90@gmail.com>
2019-01-27 19:19:32 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -35,12 +35,12 @@ def can_be_created_in_a_block(args = {title: "Home Alone", release_date: "1990"}
 end
 
 def can_get_the_first_item_in_the_database
-# return the first user
-movie = Movie.first
+  # return the first user
+  movie = Movie.first
 end
 
 def can_get_the_last_item_in_the_database
-  __
+  movie = Movie.last
 end
 
 def can_get_size_of_the_database
